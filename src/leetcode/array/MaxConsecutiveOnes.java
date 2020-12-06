33f46b5a7e1dac3951dfeff2c6b6ec8a30c7676c
Sanket Gupta <sanketgupta07@gmail.com>
2020-12-07 00:57:17 +0530

    first commit

---
+++ src/leetcode/array/MaxConsecutiveOnes.java
@@ -0,0 +1,23 @@
+/**
+ * Input: [1,1,0,1,1,1]
+ * Output: 3
+ * Explanation: The first two digits or the last three digits are consecutive 1s.
+ *  The maximum number of consecutive 1s is 3.
+ */
+package leetcode.array;
+
+/**
+ * @author sanket
+ *
+ */
+public class MaxConsecutiveOnes {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
