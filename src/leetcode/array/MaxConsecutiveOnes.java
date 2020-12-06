6466e84eaf3e898643afdb7983c8d5e9fa662ed4
Sanket Gupta <sanketgupta07@gmail.com>
2020-12-07 01:30:33 +0530

    added code

--- src/leetcode/array/MaxConsecutiveOnes.java
+++ src/leetcode/array/MaxConsecutiveOnes.java
@@ -16,8 +16,26 @@ public class MaxConsecutiveOnes {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int input[]= {1,1,0,1,1,1};
+		int output;
+		output = findMaxConsecutiveOnes(input);
+		System.out.println(output);
+	}
 
+	public static int findMaxConsecutiveOnes(int[] nums) {
+		int max=0;
+		int currentCount=0;
+		for(int num:nums) {			
+			if(num==1) {
+				currentCount++;
+			}else {
+				currentCount=0;
+			}
+			if(currentCount>max) {
+				max=currentCount;
+			}
+		}
+		return max;
 	}
 
 }
